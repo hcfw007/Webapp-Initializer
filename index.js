efc2f0ae4684d640a9ffcec4c0d3ebbef139f0ab
NickWang <hcfw007@126.com>
2019-05-24 17:37:05 +0800

    feat(index): add package.json template

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
 const inquirer = require('./lib/inquirer.js')
+const fs = require('fs')
+const path = require('path')
+const packageJson = require('./config/package-base.json')
 
 clear()
 
@@ -13,4 +16,21 @@ console.log(
 
 inquirer.askProjectInfo().then(answers => {
     console.log(answers)
+    let workDir = process.cwd()
+
+    // mkdir
+    if (process.cwd().split('/')[process.cwd().split('/').length - 1] !== answers.name) {
+        fs.mkdirSync(path.resolve(workDir, answers.name))
+        workDir = path.resolve(workDir, answers.name)
+    }
+
+    // create package.json
+    for (let item in packageJson) {
+        if (answers[item]) packageJson[item] = answers[item]
+    }
+
+    // template-specific process
+    
+    // write package.json
+    fs.writeFileSync(path.resolve(workDir, 'package.json'), JSON.stringify(packageJson, null, 2))
 })
